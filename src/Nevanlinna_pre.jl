5bc5ada304ff75ffe9b8537b19c7960f1389744f
= <=>
2023-05-22 19:05:51 +0900

    rm old functions and add calc_thetas

--- src/Nevanlinna_pre.jl
+++ src/Nevanlinna_pre.jl
@@ -1,18 +1,9 @@
 """
 Transform Nevanlinna function into contractive function.
 """
-function derive_thetas!(nev::Nevanlinna_data{T}) where T
-    for x in 1:nev.p_num
-        nev.thetas[x] = mebius(nev.nev_value[x])
-    end
-end
-
-"""
-Calculate Pick matrix elements' denominator
-"""
-function calc_pick_denominator!(nev::Nevanlinna_data{T}) where T
-    nev.pick_denominator = zeros(Complex{T},nev.p_num,nev.p_num)
-    for i in 1:nev.p_num
-        @views nev.pick_denominator[1:i,i] .= - mebius.(nev.smpl_imag_points[1:i])*conj(mebius(nev.smpl_imag_points[i]))
+function calc_thetas!(nev::NevanlinnaData{T}) where T
+    nev.thetas = Array{T}(undef,size(nev.matsu_green,1))
+    for i in eachindex(nev.matsu_green)
+        nev.thetas[i] = mebius(- nev.matsu_green[i])       
     end
 end
