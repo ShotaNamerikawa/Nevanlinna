5d2cb4a1d31012394d60df570e81bbda2fd9ecf5
= <=>
2023-05-22 19:18:07 +0900

    Bug fix

--- src/Nevanlinna_data.jl
+++ src/Nevanlinna_data.jl
@@ -1,8 +1,14 @@
 mutable struct NevanlinnaData{T}
-    matsu_freq::Array{T}
+    imag_points::Array{T}
     matsu_green::Array{T}
     thetas::Array{T}
     Schur_param::Array{T}
     N_imag::Int64
+    function NevanlinnaData(imag_points::Array{T}, matsu_green::Array{T}) where T
+        nevdata = new{T}()
+        nevdata.imag_points = imag_points
+        nevdata.matsu_green = matsu_green
+        calc_thetas!(nevdata)
+    end
 end
 
